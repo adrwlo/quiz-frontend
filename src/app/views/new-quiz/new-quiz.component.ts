96d2a1f0f4133d4ea2023d089f83ee3776448b47
adrwlo <adrwlo@o2.pl>
2024-03-06 22:30:28 +0100

    init new views

---
+++ src/app/views/new-quiz/new-quiz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-quiz',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-quiz.component.html',
+  styleUrl: './new-quiz.component.scss'
+})
+export class NewQuizComponent {
+
+}
