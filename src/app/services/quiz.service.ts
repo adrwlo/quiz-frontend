fec33956bd27fdc8072688ee69e0a4025fb555a4
adrwlo <adrwlo@o2.pl>
2024-03-09 14:28:44 +0100

    init quiz service

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { QuizData } from '../models/QuizData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  getQuizzes(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/api/getQuizzes`);
+  }
+
+  addQuiz(quiz: QuizData): Observable<QuizData> {
+    return this.http.post<QuizData>(`${this.baseUrl}/api/addQuiz`, quiz);
+  }
+
+  deleteQuiz(title: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/api/deleteQuiz/${title}`);
+  }
+}
