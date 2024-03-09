dbf81671156d6b45e50e3463d64110f0b8881442
adrwlo <adrwlo@o2.pl>
2024-03-09 12:34:12 +0100

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    RouterModule,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'quiz-frontend';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { QuizzesComponent } from './views/quizzes/quizzes.component';
+import { NewQuizComponent } from './views/new-quiz/new-quiz.component';
+import { ScoresComponent } from './views/scores/scores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quizzes', component: QuizzesComponent },
+    { path: 'new-quiz', component: NewQuizComponent },
+    { path: 'scores', component: ScoresComponent },
+];
