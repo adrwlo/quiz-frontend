9e4722f689d658b2507d6e8d7075bfca5937c40b
adrwlo <adrwlo@o2.pl>
2024-03-11 22:42:18 +0100

    added new route 'quizzes/:id/:step'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './views/home/home.component';
 import { QuizzesComponent } from './views/quizzes/quizzes.component';
 import { NewQuizComponent } from './views/new-quiz/new-quiz.component';
 import { ScoresComponent } from './views/scores/scores.component';
+import { QuizComponent } from './views/quizzes/quiz/quiz.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'quizzes', component: QuizzesComponent },
+    { path: 'quizzes/:id/:step', component: QuizComponent },
     { path: 'new-quiz', component: NewQuizComponent },
     { path: 'scores', component: ScoresComponent },
 ];
