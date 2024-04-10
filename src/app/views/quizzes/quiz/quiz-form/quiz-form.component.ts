474c17bb1ebfb2d609d9e2f473b247a7d1984c15
adrwlo <adrwlo@o2.pl>
2024-04-10 17:03:36 +0200

    update

--- src/app/views/quizzes/quiz/quiz-form/quiz-form.component.ts
+++ src/app/views/quizzes/quiz/quiz-form/quiz-form.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 import { QuizzesService } from '../../quizzes.service';
 import { FormsModule } from '@angular/forms';
+import { QuizService } from '../../../../services/quiz.service';
+import { QuizData } from '../../../../models/QuizData';
+import { QuizQuestion } from '../../../../models/QuizQuestion';
 
 @Component({
   selector: 'app-quiz-form',
@@ -10,17 +13,46 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './quiz-form.component.html',
   styleUrl: './quiz-form.component.scss'
 })
-export class QuizFormComponent {
+export class QuizFormComponent implements OnInit {
+  id!: number;
+  step!: number;
+
+  chosenQuiz!: QuizData;
+
+  answers: QuizQuestion[] =[];
+
   constructor(
     public quizzesService: QuizzesService,
+    private quizService: QuizService,
     public route: ActivatedRoute
-  ) {
-    // this.route.params.subscribe(params => {
-    //   this.quizzesService.id = params['id'];
-    //   this.quizzesService.step = params['step'];
-    // });
-
-    console.log(this.quizzesService.step);
-  }
+  ) {}
   
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+      this.step = params['step'];
+
+      console.log(this.id, this.step);
+
+      this.loadQuiz();
+    })
+  }
+
+  private loadQuiz(): void {
+    this.quizService.getQuizzes().subscribe((quizzes: QuizData[]) => {
+      if (quizzes) {
+        const foundQuiz = quizzes.find(quiz => quiz.id == this.id);
+  
+        console.log(foundQuiz);
+    
+        if (foundQuiz) {
+          this.chosenQuiz = foundQuiz;
+        } else {
+          console.error(`Quiz with id ${this.id} not found.`);
+        }
+      } else {
+        console.error(`Quizzes array is not defined.`);
+      }
+    });
+  }
 }
--- src/app/views/quizzes/quizzes.service.ts
+++ src/app/views/quizzes/quizzes.service.ts
@@ -12,103 +12,40 @@ interface Answer {
   providedIn: 'root'
 })
 export class QuizzesService {
-  quizzes: QuizData[] = [];
-  chosenQuiz!: QuizData;
-  id: number | undefined;
   step: number = 0;
-  selectedAnswer!: string; 
-  selectedAnswers: Answer[] = [];
+  quizId!: number;
 
   constructor(
-    private quizService: QuizService,
     private router: Router,
-    private route: ActivatedRoute
-  ) {
-    this.getQuizzes();
-  }
-
-  test() {
-    console.log(this.selectedAnswer);
-    this.refresh();
-  }
-
-  getQuizzes() {
-    this.quizService.getQuizzes().subscribe((data: QuizData[]) => {
-      this.quizzes = data;
-      console.log(this.quizzes);
-    })
-  }
-
-  getChosenQuiz(id: number) {
-    if (this.quizzes) {
-      const foundQuiz = this.quizzes.find(quiz => quiz.id === id);
-  
-      if (foundQuiz) {
-        this.chosenQuiz = foundQuiz;
-        console.log(this.chosenQuiz);
-      } else {
-        console.error(`Quiz with id ${id} not found.`);
-      }
-    } else {
-      console.error(`Quizzes array is not defined.`);
-    }
-  }
+  ) {}
 
   next() {
-    if (this.step < this.quizzes[0].quizQuestionDTOs.length) {
+    if (this.step > 5) {
       this.step++;
-      this.refresh();
-      console.log("ID: " + this.id);
-      console.log("STEP: " + this.step);
-      console.log("SELECTED ANSWER: " + this.selectedAnswer);
+   
       this.changedPath();
     } else { 
       return;
     }
-
-    console.log(this.step);
   }
 
   previous() {
     if (this.step > 0) {
       this.step--;
-      console.log("ID: " + this.id);
-      console.log("STEP: " + this.step);
-      console.log("SELECTED ANSWER: " + this.selectedAnswer);
       this.changedPath();
     } else {
       return;
     }
-
-    console.log(this.step);
   }
 
   cancel() {
-    console.log("ID: " + this.id);
-    console.log("STEP: " + this.step);
-    console.log("SELECTED ANSWER: " + this.selectedAnswer);
-    //ADD CLEARING ALL DATA
-    //this.router.navigate(['/quizzes']);
   }
 
   submit() {
 
   }
 
-  changeStep(step: number) {
-    this.step = step;
-    console.log(this.step);
-  }
-
   changedPath() {
-    this.router.navigate([`quizzes/${this.id}/${this.step + 1}`])
-  }
-
-  refresh() {
-    this.selectedAnswers[this.step].question = this.chosenQuiz.quizQuestionDTOs[this.step].question;
-    this.selectedAnswers[this.step].selectedAnswer = this.selectedAnswer;
-
-    console.log("SELECTED ANSWERS OBJ");
-    console.log(this.selectedAnswers);
+    this.router.navigate([`quizzes/${this.quizId}/${this.step + 1}`])
   }
 }
