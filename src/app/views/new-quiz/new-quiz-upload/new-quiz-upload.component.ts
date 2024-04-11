e249f965177cbfeb11459c86b8e292a1e0b1fb37
adrwlo <adrwlo@o2.pl>
2024-04-11 09:30:22 +0200

    new quiz ui changes

--- src/app/views/new-quiz/new-quiz-upload/new-quiz-upload.component.ts
+++ src/app/views/new-quiz/new-quiz-upload/new-quiz-upload.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface newQuizUploadFileInfo {
+  success: string;
+  error: string;
+}
+
 @Component({
   selector: 'app-new-quiz-upload',
   standalone: true,
@@ -8,5 +13,23 @@ import { Component } from '@angular/core';
   styleUrl: './new-quiz-upload.component.scss'
 })
 export class NewQuizUploadComponent {
+  isDisabled: boolean = true;
 
+  uploadFileTxt(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const fileContent = reader.result as string;
+      console.log("File content:", fileContent);
+    };
+
+    reader.onerror = () => {
+      console.error("Error occurred while reading the file.");
+    };
+
+    if (file) {
+      reader.readAsText(file);
+    }
+  }
 }
