f90dfc247554a3a77ffaa44541a173fd138d6484
adrwlo <adrwlo@o2.pl>
2024-05-16 19:28:46 +0200

    quizzes refactor

--- src/app/views/quizzes/quizzes.component.ts
+++ src/app/views/quizzes/quizzes.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuizzesService } from './quizzes.service';
-import { QuizData } from '../../models/QuizData';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quizzes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './quizzes.component.html',
   styleUrl: './quizzes.component.scss'
 })
