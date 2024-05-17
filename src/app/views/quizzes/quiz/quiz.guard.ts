f963109ff3ec52748c009e84dd23cf7b15f0d8c6
adrwlo <adrwlo@o2.pl>
2024-05-17 23:53:28 +0200

    added conditional in quiz guard

--- src/app/views/quizzes/quiz/quiz.guard.ts
+++ src/app/views/quizzes/quiz/quiz.guard.ts
@@ -3,7 +3,11 @@ import { QuizComponent  } from './quiz.component';
 
 export const QuizGuard: CanDeactivateFn<QuizComponent> = (component, currentRoute, currentState, nextState) => {
   const quizzesService = component.quizzesService;
-  quizzesService.setDefaultValues();
 
-  return confirm('Are you sure you want to leave this page? The filled quiz will be lost.');
+  if (!quizzesService.completionMode) {
+    quizzesService.setDefaultValues();
+    return confirm('Are you sure you want to leave this page? The filled quiz will be lost.');
+  } else {
+    return true; 
+  }
 };
