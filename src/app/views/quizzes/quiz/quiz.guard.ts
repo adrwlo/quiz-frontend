592aee1d744869c9fe67aab5a24d06996b276902
adrwlo <adrwlo@o2.pl>
2024-05-22 18:03:21 +0200

    fix: correct logic in QuizGuard to reset values and confirm navigation

--- src/app/views/quizzes/quiz/quiz.guard.ts
+++ src/app/views/quizzes/quiz/quiz.guard.ts
@@ -1,13 +1,14 @@
 import { CanDeactivateFn } from '@angular/router';
-import { QuizComponent  } from './quiz.component';
+import { QuizComponent } from './quiz.component';
 
 export const QuizGuard: CanDeactivateFn<QuizComponent> = (component, currentRoute, currentState, nextState) => {
   const quizzesService = component.quizzesService;
 
-  if (!quizzesService.completionMode) {
+  if (quizzesService.completionMode) {
+    quizzesService.setDefaultValues();
+    return true;
+  } else {
     quizzesService.setDefaultValues();
     return confirm('Are you sure you want to leave this page? The filled quiz will be lost.');
-  } else {
-    return true; 
-  }
+  } 
 };
