04db9afd2f753cb22bcb47960701e912fd9c7ea0
adrwlo <adrwlo@o2.pl>
2024-05-22 18:00:36 +0200

    added pagination in scores component

--- src/app/views/scores/scores.component.ts
+++ src/app/views/scores/scores.component.ts
@@ -9,28 +9,60 @@ import { DateTimeFormatPipe } from '../../pipes/date-time-format.pipe';
   standalone: true,
   imports: [CommonModule, DateTimeFormatPipe],
   templateUrl: './scores.component.html',
-  styleUrl: './scores.component.scss',
+  styleUrls: ['./scores.component.scss'],
 })
 export class ScoresComponent implements OnInit {
   ratings: Rating[] = [];
   filteredRatings: Rating[] = [];
+  paginatedRatings: Rating[] = [];
+
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalItems: number = 0;
 
   constructor(private quizService: QuizService) {}
 
   ngOnInit(): void {
     this.quizService.getRatings().subscribe((data) => {
       this.ratings = data;
-      this.filteredRatings = data;
+      this.filteredRatings = this.ratings; 
+      this.totalItems = this.ratings.length;
+      this.updatePage();
     });
   }
 
-  filter(event: any): void {
-    const value = event.target.value;
-   
-    this.filteredRatings = this.ratings.filter((rating) => {
-      return (
-        rating.quizTitle.toLowerCase().includes(value)
-      );
-    });
+  filter(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const value = inputElement?.value?.toLowerCase() || '';
+
+    this.filteredRatings = this.ratings.filter((rating) =>
+      rating.quizTitle.toLowerCase().includes(value)
+    );
+
+    this.totalItems = this.filteredRatings.length;
+    this.currentPage = 1;
+    this.updatePage();
+  }
+
+  updatePage(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedRatings = this.filteredRatings.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    if (page < 1 || page > this.totalPages) return;
+    this.currentPage = page;
+    this.updatePage();
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(this.totalPages)
+      .fill(0)
+      .map((_, i) => i + 1);
   }
 }
